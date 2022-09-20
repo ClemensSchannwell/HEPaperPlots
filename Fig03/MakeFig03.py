3c388cdd1389d3d2c997f0a0651caf59a521aa13
ClemensSchannwell <ClemensSchannwell@users.noreply.github.com>
2022-09-20 10:40:25 +0200

    adapted plot for extended simulation period

--- Fig03/MakeFig03.py
+++ Fig03/MakeFig03.py
@@ -14,16 +14,19 @@ def Main():
     LegendFSize=24
     AxisFSize=27
     TitleFSize=31
-    YLim=[0,6e8]
+    YLim=[0,6e12]
     YTickLabelSize=24
     fig,ax = CreateSuplotAxesSimple(1,1,10,15)
     PlotFiles=CreateTSInputAnom("reference","Hudson")
-    PlotPISMTimeSeries(PlotFiles,"flux_crossSection",ax)
+    PlotPISMTimeSeries(PlotFiles,"flux_crossSection",ax,20)
     PlotFiles=CreateTSInputAnom("reference","Kenzie")
-    PlotPISMTimeSeries(PlotFiles,"flux_crossSection",ax)
-    ax.set_ylabel("Ice flux [m$^2$/yr]",fontsize=AxisFSize)
+    PlotPISMTimeSeries(PlotFiles,"flux_crossSection",ax,20)
+    # ax.fill_between([20,88],[-100,-100],[9e12,9e12],
+                # color="grey", hatch = '//', label='Analysis Period',
+                # facecolor = 'none',alpha=0.6)
+    ax.set_ylabel("Ice flux [m$^3$/yr]",fontsize=AxisFSize)
     # ax.set_xlim([-65,-7.5])
-    ax.set_xlim([0,57.6])
+    ax.set_xlim([0,65.0])
     ax.set_ylim(YLim)
     ax.legend(loc=1,prop={'size':LegendFSize})
     ax.text(0.21, 0.95, "Control simulation (Ctrl)",color="black",
