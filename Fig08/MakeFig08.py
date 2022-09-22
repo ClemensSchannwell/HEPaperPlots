cc84d71e271bcb12de169061d8e77028a5bf616e
ClemensSchannwell <ClemensSchannwell@users.noreply.github.com>
2022-09-22 09:24:36 +0200

    updated to restrict analysis to the time from -45 to -7.5 kyrs

--- Fig08/MakeFig08.py
+++ Fig08/MakeFig08.py
@@ -12,16 +12,17 @@ from UtilityFcts import *
 
 def Main():
     Text = ["Ctrl", "SMB-", "Geo-","ST-", "SMB+", "Geo+", "ST+"]
-    TextPos = [[6600, 1.00e15],[12300, 1.09e15], [8100, 1.16e15], [7400, 0.98e15], \
-            [4100, 0.59e15], [5700, 0.985e15], [6000, 0.89e15]]
-    TextPosKen = [[4500, 0.54e15],[7500, 0.38e15], [6000, 0.49e15], [5800, 0.74e15]]
+    TextPos = [[6900, 1.06e15],[10300, 1.06e15], [8300, 1.20e15], [8100, 1.10e15], \
+            [5300, 0.72e15], [4900, 0.75e15], [6000, 1.02e15]]
+    TextPosKen = [[4500, 0.59e15],[7500, 0.38e15], [6000, 0.62e15], [5700, 0.84e15]]
     Colors = ["Black", "Blue", "Red", "Purple", "Blue", "Red", "Purple" ]
-    Labels = ["Constant forcing", "Surface mass balance (SMB)", "Geothermal heatflux (Geo)", "Ice surface temperature (ST)", "SMB", "Geothermal heatflux", "Ice surface temperature"]
+    Labels = ["Constant forcing (Ctrl)", "Surface mass balance (SMB)", "Geothermal heatflux (Geo)", "Ice surface temperature (ST)", "SMB", "Geothermal heatflux", "Ice surface temperature"]
 
     [DeltaVol_Hudson, HEFreq_Hudson] = ComputeSurgeTiming4IceVol("Hudson"\
-            ,2.5e8, "anomaly")
+            ,2.0e12, "anomaly")
+    print("Now doing Mackenzie ...")
     [DeltaVol_Kenzie, HEFreq_Kenzie] = ComputeSurgeTiming4IceVol("Kenzie"\
-            ,1.0e8, "anomaly")
+            ,1.0e12, "anomaly")
     HEFreqMean_Hudson = ComputeMean(HEFreq_Hudson)
     HEFreqMean_Kenzie = ComputeMean(HEFreq_Kenzie)
     DeltaVolMean_Hudson = ComputeMean(DeltaVol_Hudson)
@@ -41,7 +42,8 @@ def Main():
         else:
             ax[0].scatter(HEFreqMean_Hudson[i],DeltaVolMean_Hudson[i],s=450,marker="o",c=Colors[i], edgecolors=Colors[i])
         ax[0].annotate(txt,(TextPos[i]),fontsize=TextFS)
-        ConfidenceEllipse(HEFreq_Hudson[i],DeltaVol_Hudson[i],ax[0],n_std=1.0,alpha=0.25,facecolor=Colors[i], edgecolor=Colors[i])
+        if len(HEFreq_Hudson[i]) > 4:
+            ConfidenceEllipse(HEFreq_Hudson[i],DeltaVol_Hudson[i],ax[0],n_std=1.0,alpha=0.25,facecolor=Colors[i], edgecolor=Colors[i])
     ax[0].set_xlabel("Surge Period [yrs]",fontsize=AxisFS)
     ax[0].set_ylabel("$\Delta$ice volume [m$^3$]",fontsize=AxisFS)
     ax[0].tick_params(axis='both',labelsize=LabelFS)
@@ -53,18 +55,19 @@ def Main():
     ax[1].set_title("Mackenzie ice stream",fontsize=TitleFS,
             fontweight='bold')
     XLim = [3500,14200]
-    YLim = [0.3e15, 1.43e15]
+    YLim = [0.3e15, 1.45e15]
     ax[0].set_xlim(XLim)
     ax[0].set_ylim(YLim)
-    ax[0].legend(scatterpoints=1,labelspacing=1.2, frameon=False,loc=3)
+    ax[0].legend(scatterpoints=1,labelspacing=1.4, frameon=False,loc=3)
     ax[1].set_xlim(XLim)
     ax[1].set_ylim(YLim)
-    ax[1].legend(scatterpoints=1,labelspacing=1.2, frameon=False,loc="best")
+    ax[1].legend(scatterpoints=1,labelspacing=1.4, frameon=False,loc="best")
     ax[0].text(0.045, 0.965, '(a)',color="black", horizontalalignment='center', \
             verticalalignment='center', transform=ax[0].transAxes,fontsize=AxisFS)
     ax[1].text(0.045, 0.965, '(b)',color="black", horizontalalignment='center', \
             verticalalignment='center', transform=ax[1].transAxes,fontsize=AxisFS)
     plt.tight_layout()
+    # plt.show()
     SavePlot("Fig08","IceVolume_PeriodPlotAnomaly",600)
 
 if __name__ == '__main__':
